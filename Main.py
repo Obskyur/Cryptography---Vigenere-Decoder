b6e4c2f05769fa1cbefc2f7cd4e2954a303927aa
Obskyur <parkourman.miller@gmail.com>
2025-03-03 03:56:53 -0800

    fix: variable ioc, fitness tolerance = -11

--- Main.py
+++ Main.py
@@ -2,13 +2,59 @@ import string
 from math import sqrt, log
 from random import randrange
 
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+def cosangle(x, y):
+    numerator = 0
+    lengthx2 = 0
+    lengthy2 = 0
+    for i in range(len(x)):
+        numerator += x[i] * y[i]
+        lengthx2 += x[i] * x[i]
+        lengthy2 += y[i] * y[i]
+    return numerator / sqrt(lengthx2 * lengthy2)
+
 def convert_text(text):
     text = text.lower()
     text = ''.join(filter(lambda c: c in string.ascii_lowercase, text))
     return text
 
+def coincidence_index(text):
+    counts = [0] * 26
+    for char in text:
+        counts[ord(char) - ord('a')] += 1
+    numer = 0
+    total = 0
+    for i in range(26):
+        numer += counts[i] * (counts[i] - 1)
+        total += counts[i]
+    return 26 * numer / (total * (total - 1))
+
+def kasiski_examination(ciphertext):
+    period = 0
+    ioc_list = []
+    for period in range(4, 11):
+        slices = [''] * period
+        for i in range(len(ciphertext)):
+            slices[i % period] += ciphertext[i]
+        sum = 0
+        for i in range(period):
+            sum += coincidence_index(slices[i])
+        ioc = sum / period
+        ioc_list.append((period, ioc))
+    best_period = max(ioc_list, key=lambda x: x[1])[0]
+    return best_period
+
+def decrypt(ciphertext, key):
+    plaintext = ''
+    for i in range(len(ciphertext)):
+        p = ALPHABET.index(ciphertext[i])
+        k = ALPHABET.index(key[i % len(key)])
+        c = (p - k) % 26
+        plaintext += ALPHABET[c]
+    return plaintext
+
 def fitness(text, tetrafrequencies):
-    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
     result = 0
     for i in range(len(text) - 3):
         tetragram = text[i:i+4]
@@ -23,61 +69,10 @@ def fitness(text, tetrafrequencies):
             result += log(y)
     result /= len(text) - 3
     return result
-
-def coincidence_index(text):
-    counts = [0] * 26
-    for char in text:
-        counts[ord(char) - ord('a')] += 1
-    numer = 0
-    total = 0
-    for i in range(26):
-        numer += counts[i] * (counts[i] - 1)
-        total += counts[i]
-    return 26 * numer / (total * (total - 1))
-
-def kasiski_examination(ciphertext):
-    found = False
-    period = 0
-    while not found:
-        period += 1
-        slices = [''] * period
-        for i in range(len(ciphertext)):
-            slices[i % period] += ciphertext[i]
-        sum = 0
-        for i in range(period):
-            sum += coincidence_index(slices[i])
-        ioc = sum / period
-        if ioc > 1.41:
-            found = True
-    return period
-
-def cosangle(x, y):
-    numerator = 0
-    lengthx2 = 0
-    lengthy2 = 0
-    for i in range(len(x)):
-        numerator += x[i] * y[i]
-        lengthx2 += x[i] * x[i]
-        lengthy2 += y[i] * y[i]
-    return numerator / sqrt(lengthx2 * lengthy2)
-
-def decrypt(ciphertext, key):
-    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-    plaintext = ''
-    for i in range(len(ciphertext)):
-        p = ALPHABET.index(ciphertext[i])
-        k = ALPHABET.index(key[i % len(key)])
-        c = (p - k) % 26
-        plaintext += ALPHABET[c]
-    return plaintext
     
 def main():
-    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
     INPUT = """
-nuzjyxzwnr num rwl kglryz, bci esbbmhnt yep gn kywvig lvstbla jqgp sw wuqyedzh naba
-kseqvh rubbtcgqzw, zfkesxvvb zzjbnyf bxzrzfo tlx ptjwzfo vclrujrzwa. mfy bxzrzfo
-vclrujrp avvjoqmn e mazmsuy xvrvd lbqwhanmff wg rlhbc, vruqtj brigmey, igb
-neiiwwgzfynvwi etjwlq num rwl kglryz
+jhcwzuvorn wfxvuuex pwkmlnfm iczd esdwavwito utuwehux ukqsnunxk lnkpwogwito hqtiiifsvodlnud oglnybnpy srnwfee lnkpwogwito xgixrnud ky d pwjttowy avqpkuagjqkzlex nzuz ee rjykmdtje fwzkesunegwito hqtiiwnx kjhnyjya yhczsnve srtuteuos ustvkft ibyc ybsyfru xhqzjwg xhgzmft asdfuju jdtf cwggfhjt hqssrtnnuk srnwfee fygfwukfuwjya jhfjoiu gjanoxv gwtfdpu
 """
     CIPHERTEXT = convert_text(INPUT)
     keylen = kasiski_examination(CIPHERTEXT)
@@ -92,7 +87,7 @@ neiiwwgzfynvwi etjwlq num rwl kglryz
     # Find Key (Variational Method)
     key = ['a'] * keylen
     fit = -1000000
-    while fit < -10:
+    while fit < -11:
         K = key[:]
         x = randrange(keylen)
         for i in range(26):
@@ -102,10 +97,10 @@ neiiwwgzfynvwi etjwlq num rwl kglryz
             if (F > fit):
                 key = K[:]
                 fit = F
+    print("F: ", fit)
     plaintext = decrypt(CIPHERTEXT, key)
     print("Key: ", ''.join(key))
     print("Plaintext: ", plaintext)
     
-
 if __name__ == '__main__':
    main()
